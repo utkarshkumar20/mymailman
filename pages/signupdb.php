61b1ddbb5dc86ee151f42c69af8cf45b0e5febac
utkarsh <utkarsh20.hestabit@gmail.com>
2022-07-15 13:22:00 +0530

    ththt

--- pages/signupdb.php
+++ pages/signupdb.php
@@ -1,9 +1,10 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+error_reporting(0);
+ini_set('display_errors', '1');
 $con = mysqli_connect('Localhost', 'tse', 'bPmtHasjyTJ2SgZJ', 'utkarsh') or die("connection failed");
-
+echo "<pre>";
+print_r($_FILES);
+die(0);
 if (isset($_POST['signup'])) {
     $fname = $_POST['fname'];
     $lname = $_POST['lname'];
