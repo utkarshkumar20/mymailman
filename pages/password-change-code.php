3afa4e234047f68b2b0005f69eade7de45d13fb7
utkarsh <utkarsh20.hestabit@gmail.com>
2022-07-19 18:23:01 +0530

    debug in edit profile

--- pages/password-change-code.php
+++ pages/password-change-code.php
@@ -8,7 +8,7 @@
         $recemail = $_POST['RecEmail'];
         if ($_POST["password"] == $_POST["conpassword"]) {
             $query = "UPDATE Signup_table  SET password = '$password'  WHERE Username = '$recemail'";
-            // die("end here");
+            die("end here");
             $data = mysqli_query($con, $query);
             if ($data) {
                 echo '<script type="text/javasczript">';
