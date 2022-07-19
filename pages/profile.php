a6c9a3252bdd5d026106f9133fb6c0b4b43aac72
utkarsh <utkarsh20.hestabit@gmail.com>
2022-07-19 17:39:18 +0530

    debug in edit profile

--- pages/profile.php
+++ pages/profile.php
@@ -6,7 +6,7 @@ session_start();
 print_r($_SESSION);
 
 $user_id = $_SESSION['id'];
-echo $query = "SELECT * FROM signup_table where id = '$userid'";
+echo $query = "SELECT * FROM signup_table where id = '$user_id'";
 $result = mysqli_query($con, $query);
 if (mysqli_num_rows($result) > 0) {
     $user_id = $result->fetch_assoc();
