aaaefbd225ea332eabc000c92e50990ef991ae41
utkarsh <utkarsh20.hestabit@gmail.com>
2022-07-19 17:40:43 +0530

    debug in edit profile

--- pages/profile.php
+++ pages/profile.php
@@ -3,15 +3,15 @@
 require('../includes/config.php');
 
 session_start();
-print_r($_SESSION);
+// print_r($_SESSION);
 
 $user_id = $_SESSION['id'];
 echo $query = "SELECT * FROM signup_table where id = '$user_id'";
 $result = mysqli_query($con, $query);
 if (mysqli_num_rows($result) > 0) {
-    $user_id = $result->fetch_assoc();
-// echo "<pre>";
-// print_r(($user_id));
+    $user_data = $result->fetch_assoc();
+echo "<pre>";
+print_r(($user_data));
 }
 die("this example");
 
