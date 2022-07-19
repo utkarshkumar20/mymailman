bbedfdb3e6b513df0d724716925cfb4e1b106462
utkarsh <utkarsh20.hestabit@gmail.com>
2022-07-19 15:53:54 +0530

    comments remove

--- pages/Editprofile.php
+++ pages/Editprofile.php
@@ -245,7 +245,7 @@ if (isset($_GET["id"])) {
     </script>
 
     <!-- ************************************************validation******************************************* -->
-
+<!-- 
     <script>
         function validation() {
 
@@ -315,7 +315,7 @@ if (isset($_GET["id"])) {
 
 
         }
-    </script>
+    </script> -->
     <!-- ******************************************************************************************************** -->
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
