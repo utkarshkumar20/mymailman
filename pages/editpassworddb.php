64cbf1f17025f66770827bc0cd9390c3821d9503
utkarsh <utkarsh20.hestabit@gmail.com>
2022-07-20 15:56:32 +0530

    add some file

---
+++ pages/editpassworddb.php
@@ -0,0 +1,27 @@
+<?php
+    include('../includes/config.php');
+    session_start();
+
+    if (isset($_POST['password_update'])) {
+        $password = $_POST['password'];
+        $conpassword = $_POST['conpassword'];
+        $username = $_POST['username'];
+        $id = $_SESSION['id'];
+        if ($_POST["password"] == $_POST["conpassword"]) {
+            $query = "UPDATE Signup_table  SET password = '$password'  WHERE Id = '$id' ";
+            $data = mysqli_query($con, $query);
+            if ($data) {
+                echo '<script type="text/javascript">';
+                echo 'alert("password successfully changed");';
+                echo 'window.location.href = "profile.php";';
+                echo '</script>';
+            } else {
+                // die("end here"); 
+                echo '<script type="text/javascript">';
+                echo 'alert("Password is wrong");';
+                echo 'window.location.href = "editpassword.php";';
+                echo '</script>';
+            }
+        }
+    }
+    ?> 
