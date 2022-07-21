ed4e90117e13e5e44bf50b2a5cee9c9062d7ef1e
utkarsh <utkarsh20.hestabit@gmail.com>
2022-07-21 15:29:21 +0530

    some change in login page

--- pages/index.php
+++ pages/index.php
@@ -1,3 +1,6 @@
+<?php
+include('../includes/config.php');
+?>
 <!doctype html>
 <html lang="en">
 
@@ -98,7 +101,8 @@
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <script src="../js/sweetalert.js"></script>
-    <?php
+   
+   <?php
     if (isset($_SESSION['status']) && $_SESSION['status'] != '') {
     ?>
         <script>
